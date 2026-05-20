ea797e5485bdeeaabdf62f7e1fd2a705b827b75e
sm-lucas <133544611+sm-lucas@users.noreply.github.com>
2026-05-20 20:48:52 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GerenciadorInvestimentos
+{
+    // Modelo do Ativo Financeiro
+    class Ativo
+    {
+        public string Ticket { get; set; } // Ex: PETR4, IVVB11
+        public int Quantidade { get; set; }
+        public double PrecoMedio { get; set; }
+        public double PrecoAtual { get; set; }
+
+        public double TotalInvestido => Quantidade * PrecoMedio;
+        public double ValorAtualizado => Quantidade * PrecoAtual;
+        public double LucroPrejuizo => ValorAtualizado - TotalInvestido;
+
+        public override string ToString()
+        {
+            return $"{Ticket.ToUpper()} | Qtd: {Quantidade} | P.Médio: R${PrecoMedio:F2} | P.Atual: R${PrecoAtual:F2} | Rendimento: R${LucroPrejuizo:F2}";
+        }
+    }
+ }
